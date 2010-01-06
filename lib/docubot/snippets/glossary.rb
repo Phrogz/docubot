b0fd5cb544a8eebdbe3102ac77569b16bf925e42
Gavin Kistner <!@phrogz.net>
2010-01-06 14:19:26 -0700

    Premature optimization on glossary snippet

--- lib/docubot/snippets/glossary.rb
+++ lib/docubot/snippets/glossary.rb
@@ -1,6 +1,5 @@
 pattern = /\$\$(\w[\w ]*\w)\$\$/
 DocuBot.handle_snippet pattern do |match|
-	term = match[pattern,1]
 	# TODO: look up glossary terms, include information
-	"<span class='glossary'>#{term}</span>"
+	"<span class='glossary'>#{match[ 2..-3 ]}</span>"
 end

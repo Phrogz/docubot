51688d9652ab4aeed53150179d96c18aceccf20d
Gavin Kistner <!@phrogz.net>
2010-01-11 12:25:36 -0700

    Force BlueCloth to UTF-8 until it is fixed.

--- lib/docubot/converters/markdown.rb
+++ lib/docubot/converters/markdown.rb
@@ -4,4 +4,9 @@ require 'bluecloth'
 class BlueCloth
 	extend DocuBot::Converter
 	converts_for :md, :markdown
+	alias_method :bc_to_html, :to_html
+	def to_html
+		# BlueCloth 2.0.5 takes UTF-8 source and returns ASCII-8BIT
+		bc_to_html.encode( 'UTF-8', :undef=>:replace )
+	end
 end

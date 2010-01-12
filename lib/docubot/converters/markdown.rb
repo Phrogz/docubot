e4921b90f31c30e94f5c7b23db2a1785ac3e92a9
Gavin Kistner <!@phrogz.net>
2010-01-12 13:59:19 -0700

    Nice error messages for skipping Textile and Markdown

--- lib/docubot/converters/markdown.rb
+++ lib/docubot/converters/markdown.rb
@@ -1,14 +1,19 @@
 # encoding: UTF-8
 require 'rubygems'
-require 'bluecloth'
-class BlueCloth
-	extend DocuBot::Converter
-	converts_for :md, :markdown
-	alias_method :bc_to_html, :to_html
-	def to_html
-		# BlueCloth 2.0.5 takes UTF-8 source and returns ASCII-8BIT
-		result = bc_to_html
-		result.encode!( 'UTF-8', :undef=>:replace ) if Object.const_defined? "Encoding"
-		result
+begin
+	require 'bluecloth'
+	class BlueCloth
+		extend DocuBot::Converter
+		converts_for :md, :markdown
+		alias_method :bc_to_html, :to_html
+		def to_html
+			# BlueCloth 2.0.5 takes UTF-8 source and returns ASCII-8BIT
+			result = bc_to_html
+			result.encode!( 'UTF-8', :undef=>:replace ) if Object.const_defined? "Encoding"
+			result
+		end
 	end
+rescue LoadError
+	warn "Unable to load RedCloth gem; textile markup will not be recognized as a page."
 end
+
--- lib/docubot/converters/textile.rb
+++ lib/docubot/converters/textile.rb
@@ -1,17 +1,18 @@
 # encoding: UTF-8
 require 'rubygems'
 begin
-require 'redcloth'
-module RedCloth
-	extend DocuBot::Converter
-	converts_for :textile, :rc
-
-	class << self
-		alias_method :rc_new, :new
-		def new( markup )
-			rc_new( markup, [:no_span_caps] )
+	require 'redcloth'
+	module RedCloth
+		extend DocuBot::Converter
+		converts_for :textile, :rc
+	
+		class << self
+			alias_method :rc_new, :new
+			def new( markup )
+				rc_new( markup, [:no_span_caps] )
+			end
 		end
 	end
-end
 rescue LoadError
+	warn "Unable to load RedCloth gem; textile markup will not be recognized as a page."
 end

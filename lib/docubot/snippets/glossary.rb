9dbaa65d1217cf0904e8ca2763b3db8989e60086
Gavin Kistner <gavin@phrogz.net>
2010-01-14 07:25:45 -0700

    Glossary entries record term for lookup

--- lib/docubot/snippets/glossary.rb
+++ lib/docubot/snippets/glossary.rb
@@ -4,7 +4,7 @@ DocuBot.handle_snippet pattern do |match, page|
   parts = match[ 2..-3 ].split(':',2)
   text, term = parts.first, parts.last
   if page.bundle.glossary[ term ]
-  	"<span class='glossary'>#{text}</span>"
+  	"<span class='glossary' term='#{term}'>#{text}</span>"
   else
     page.bundle.glossary.add_missing_term( term )
   	"<span class='missing'>#{text}</span>"
